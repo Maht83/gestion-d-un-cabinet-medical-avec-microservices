799c7cbf27049b6b7abee3c0e47b5a99cb2386e5
Maht83 <mattheovasseur6@gmail.com>
2025-02-19 14:23:07 +0100

    Mise en place de Eureka plus de la configuration en Java 1.8

---
+++ praticien/src/test/java/com/praticien/PraticienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
